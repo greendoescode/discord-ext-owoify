909284917d21d20a85e039134329e4fd78d476dd
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-04 20:37:44 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ setup(name='discord-ext-owoify',
       version=version,
       packages=['discord.ext.owoify'],
       license='MIT',
+      download_url = 'https://github.com/GreenDiscord/discord-ext-owoify/archive/v0.0.2-a.tar.gz',
+      install_requires=['discord.py'],
       description='An extension module to owoify text',
       install_requires=['discord.py>=1.2.5'],
       python_requires='>=3.5.3'
