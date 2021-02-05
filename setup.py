8b256b368dbb0c46491181acb460ec71e8bc610b
GreenDiscord <77177015+GreenDiscord@users.noreply.github.com>
2021-02-05 18:50:56 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,18 @@
 from setuptools import setup
 import re
+import readme_renderer
 
-version = ''
-with open('discord/ext/owoify/__init__.py') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+version = '0.0.2.1-a'
 
-if not version:
-    raise RuntimeError('version is not set')
-
-if version.endswith(('a', 'b', 'rc')):
-    # append version identifier based on commit count
-    try:
-        import subprocess
-        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        if out:
-            version += out.decode('utf-8').strip()
-        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        if out:
-            version += '+g' + out.decode('utf-8').strip()
-    except Exception:
-        pass
 
 setup(name='discord-ext-owoify',
       author='Green',
-      url='https://github.com/GreenDiscorx/discord-ext-owoify',
+      url='https://github.com/GreenDiscord/discord-ext-owoify',
       version=version,
       packages=['discord.ext.owoify'],
       license='MIT',
-      download_url = 'https://github.com/GreenDiscord/discord-ext-owoify/archive/v0.0.2-a.tar.gz',
-      install_requires=['discord.py'],
+      long_description=readme_renderer.markdown.render("### discord-ext-owoify\n Simple ext package for owoifing text.\n ###Install\n Simple paste this into your terminal\n ```py python -m pip install discord-ext-owoify```\n ###Simple Use\n Firstly, import the package using\n ```py from discord.ext import owoify```"),
+      download_url = 'https://github.com/GreenDiscord/discord-ext-owoify/archive/v0.0.2.1-a.tar.gz',
       description='An extension module to owoify text',
       install_requires=['discord.py>=1.2.5'],
       python_requires='>=3.5.3'
